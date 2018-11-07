ef790add80eba71f5758551a31c91244a63ac2ab
gezahegnw <gezahegnw@gmail.com>
2018-11-07 14:06:45 -0600

    first push

---
+++ app/data/friends.js
@@ -0,0 +1,45 @@
+var friends = [ {
+    "name" : "Matheow",
+    "scores" : [5,4,2,5,1,1,2,1,3,5]
+}, {
+    "name" : "Smith",
+    "scores" : [3,4,2,4,4,1,1,2,3,4]
+}, {
+    "name" : "Dived",
+    "scores" : [5,5,5,5,5,5,5,5,5,5]
+}, {
+    "name" : "Luke",
+    "scores" : [5,2,4,3,3,2,1,3,3,4]
+}, {
+    "name" : "Mark",
+    "scores" : [4,1,5,3,4,5,2,3,4,5]
+}, {
+    "name" : "Lessa",
+    "scores" : [4,1,5,3,4,5,2,3,4,5]
+}, {
+    "name" : "Rose",
+    "scores" : [3,4,2,4,4,1,1,2,3,4]
+}, {
+    "name" : "Jennfer",
+    "scores" : [5,4,2,5,1,1,2,1,3,5]
+}, {
+    "name" : "",
+    "scores" : [1,5,1,3,5,1,4,3,2,1]
+}, {
+    "name" : "James",
+    "scores" : [1,3,3,1,2,4,4,1,3,1]
+}, {
+    "name" : "Bob",
+    "scores" : [5,2,4,3,3,2,1,3,3,4]
+}, {
+    "name" : "Dan",
+    "scores" : [1,1,1,1,1,1,1,1,1,1]
+}, {
+    "name" : "Chris",
+    "scores" : [2,3,5,5,3,5,2,1,3,2]
+}, {
+    "name" : "Stive",
+    "scores" : [2,3,5,5,3,5,2,1,3,2]
+}];
+
+module.exports = friends;
